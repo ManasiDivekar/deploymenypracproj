447541ab75a238d733a7209dba7cab07816c8ae2
MansiDivekar <mansidivekar2000@gmail.com>
2023-03-14 10:28:11 +0530

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeploymentComponent } from './deployment/deployment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'navbar',
+    component:DeploymentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
